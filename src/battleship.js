cbdb20dcce3072af4594d93d85f63c2d1b03649f
Xonelist <83855246+Xonelist@users.noreply.github.com>
2024-01-18 23:08:51 +0700

    Add funtion receiveAttack that record and validate ship location

--- src/battleship.js
+++ src/battleship.js
@@ -1,9 +1,49 @@
 class GameBoard {
+    constructor() {
+        this.board = {};
+        this.Ships = null;
+        this.AttackedRecords = {}
+    }
 
+    addShips(type, x, y) {
+        let ship;
+        if(`${x}, ${y}` in this.board) return false;
+        switch (type) {
+            case 'P':
+                ship = new PatrolBoat()
+                break;
+            case 'S':
+                ship = new Submarine()
+                break;
+            case 'D':
+                ship = new Destroyer()
+                break;
+            case 'B':
+                ship = new Battleship()
+                break;
+            case 'C':
+                ship = new Carrier()
+                break;
+                
+            default:
+                return false;
+        }
+        this.board[`${x}, ${y}`] = ship;
+    }
+
+    receiveAttack(x, y) {
+        if(`${x}, ${y}` in this.AttackedRecords) return false;
+        this.AttackedRecords[`${x}, ${y}`] = true;
+        if(`${x}, ${y}` in this.board) {
+            return this.board[`${x}, ${y}`].hit();
+        } else {
+            return false;
+        }
+    }
 }
 
 class Ship {
-    constructor(player) {
+    constructor(player = 'St John') {
         this.player = player;
         this.totalHit = 0;
         this.location = null;
@@ -16,9 +56,11 @@ class Ship {
 
     isSunk() {
         if(this.totalHit === this.size) {
-            return `${this.player}'s ${this.shipName} is sunk`
+            //return `${this.player}'s ${this.shipName} is sunk`
+            return true;
         } else {
-            return `${this.totalHit} / ${this.size} hits till ${this.player}'s ${this.shipName} sinking`
+            //return `${this.totalHit} / ${this.size} hits till ${this.player}'s ${this.shipName} sinking`
+            return false;
         }
     }
 }
@@ -62,4 +104,4 @@ class Carrier extends Ship {
     }
 }
 
-module.exports =  { Ship, PatrolBoat, Submarine, Destroyer, Battleship, Carrier, Player }
+module.exports =  { Ship, PatrolBoat, Submarine, Destroyer, Battleship, Carrier, GameBoard}
