b6b18a28d5457b8015b3664d0ad3978e7669e51d
Xonelist <83855246+Xonelist@users.noreply.github.com>
2024-01-18 23:43:55 +0700

    Jest for isAllShipsSunk

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -36,8 +36,30 @@ test('Incoming attack on 0, 0 location (Hit)', ()=> {
     expect(board.receiveAttack(0, 0)).toBe('St John\'s Patrol Boat got hit');
 });
 
+test('Ship got hit twice in same location (total hit = 1)', ()=> {
+    const board = new GameBoard()
+    const ship = board.addShips('P', 0, 0);
+    board.receiveAttack(0, 0);
+    board.receiveAttack(0, 0);
+    expect(ship.totalHit).toBe(1);
+});
+
 test('Incoming attack on 0, 1 location (miss)', ()=> {
     const board = new GameBoard()
     board.addShips('P', 0, 0);
     expect(board.receiveAttack(0, 1)).toBe(false);
+});
+
+test('is All Ship in board sunk? (false)', ()=> {
+    const board = new GameBoard()
+    board.addShips('P', 0, 0);
+    expect(board.isAllShipSunk()).toBe(false);
+});
+
+test('is All Ship in board sunk? (true)', ()=> {
+    const board = new GameBoard()
+    board.addShips('P', 0, 0);
+    board.receiveAttack(0, 0);
+    board.receiveAttack(1, 0);
+    expect(board.isAllShipSunk()).toBe(true);
 });
