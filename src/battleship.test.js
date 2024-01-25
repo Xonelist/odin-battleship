153f1aa526c6c7e272398ff2c20f9df50dd009c3
Xonelist <83855246+Xonelist@users.noreply.github.com>
2024-01-26 06:35:33 +0700

    Jest test for players battle

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -1,4 +1,4 @@
-const {Ship, PatrolBoat, Submarine, Destroyer, Battleship, Carrier, GameBoard} = require('./battleship');
+const {Ship, PatrolBoat, Submarine, Destroyer, Battleship, Carrier, GameBoard,Player, ComputerAI} = require('./battleship');
 
 
 //Ship class Test
@@ -26,19 +26,19 @@ const {Ship, PatrolBoat, Submarine, Destroyer, Battleship, Carrier, GameBoard} =
 test('There is a ship at 0, 0', ()=> {
     const cordinate = `0, 0`
     const board = new GameBoard()
-    board.addShips('P', 0, 0);
+    board.addShip('P', 0, 0);
     expect(board.board[cordinate].shipName).toBe('Patrol Boat')
 });
 
 test('Incoming attack on 0, 0 location (Hit)', ()=> {
     const board = new GameBoard()
-    board.addShips('P', 0, 0);
+    board.addShip('P', 0, 0, 'St John');
     expect(board.receiveAttack(0, 0)).toBe('St John\'s Patrol Boat got hit');
 });
 
 test('Ship got hit twice in same location (total hit = 1)', ()=> {
     const board = new GameBoard()
-    const ship = board.addShips('P', 0, 0);
+    const ship = board.addShip('P', 0, 0);
     board.receiveAttack(0, 0);
     board.receiveAttack(0, 0);
     expect(ship.totalHit).toBe(1);
@@ -46,20 +46,28 @@ test('Ship got hit twice in same location (total hit = 1)', ()=> {
 
 test('Incoming attack on 0, 1 location (miss)', ()=> {
     const board = new GameBoard()
-    board.addShips('P', 0, 0);
+    board.addShip('P', 0, 0);
     expect(board.receiveAttack(0, 1)).toBe(false);
 });
 
 test('is All Ship in board sunk? (false)', ()=> {
     const board = new GameBoard()
-    board.addShips('P', 0, 0);
+    board.addShip('P', 0, 0);
     expect(board.isAllShipSunk()).toBe(false);
 });
 
 test('is All Ship in board sunk? (true)', ()=> {
     const board = new GameBoard()
-    board.addShips('P', 0, 0);
+    board.addShip('P', 0, 0);
     board.receiveAttack(0, 0);
     board.receiveAttack(1, 0);
     expect(board.isAllShipSunk()).toBe(true);
+});
+
+//Battle Test
+test('Computer Patrol Boat got hit by Player at 4, 5? (true)', ()=> {
+    const player = new Player('Player1')
+    const computer = new ComputerAI('Computer')
+    computer.init();
+    expect(player.attack(computer, 5, 4)).toBe('Computer\'s Patrol Boat got hit');
 });
