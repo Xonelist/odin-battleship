181b34a7492ec2aa47013752e64036fb29416fa0
Xonelist <83855246+Xonelist@users.noreply.github.com>
2024-02-13 16:44:30 +0700

    every player move addEvent to each cell

--- src/DOM.js
+++ src/DOM.js
@@ -1,7 +1,6 @@
-import { maingame } from "./maingame";
 import { ComputerAI, Player } from "./player";
 
-function mainmenu() {
+function mainmenu(player1, player2) {
     const div = document.createElement('div');
     const introduction = document.createElement('h1');
     introduction.className = 'Intro-title'
@@ -11,21 +10,19 @@ function mainmenu() {
     startbutton.textContent = 'Start Game'
 
     startbutton.addEventListener('click',() => {
-        _wait = false;
+        document.body.removeChild(div);
+        maingame();
+        game();
     })
 
     div.appendChild(introduction);
     div.appendChild(startbutton);
     document.body.appendChild(div);
-
-    while(_wait) {
-
-    }
-    console.log('you clicked')
 }
 
-function game() {
+function maingame() {
     const body = document.body
+    const main = document.createElement('main')
     const divHead = document.createElement('div')
     divHead.id = 'divHead'
     const divBody = document.createElement('div')
@@ -35,28 +32,14 @@ function game() {
     const divBoard = document.createElement('div');
     divBoard.id = 'divBoard';
 
-    const player1 = new Player('Player1')
-    const player2 = new ComputerAI('ComputerAI');
-
-    let noWinner = true;
-    let winner = '';
-    let activePlayer = player1;
-    let oppenentPlayer = player2;
-
-    player1.init();
-    player2.init();
-
-
-    divBoard.appendChild(showBoard(player1.name));
-    divBoard.appendChild(showBoard(player2.name));
-
     divBody.appendChild(divBoard)
 
-    body.appendChild(divHead)
-    body.appendChild(divBody)
-    body.appendChild(divFoot)
-}
+    main.appendChild(divHead)
+    main.appendChild(divBody)
+    main.appendChild(divFoot)
 
+    body.appendChild(main);
+}
 
 //return board with form of table
 function showBoard(playerName) {
@@ -67,6 +50,7 @@ function showBoard(playerName) {
         for(let j = 0; j < 10; j++) {
             const col = document.createElement('td');
             col.id = `c${j}${i}`;
+            col.classList = 'cell'
             row.appendChild(col)
         }
         table.insertBefore(row, table.firstChild);
@@ -75,4 +59,49 @@ function showBoard(playerName) {
     return table;
 }
 
-export { mainmenu, showBoard, game }
+//player turns display
+function turnBoards(playerName) {
+    const head = document.querySelector('#divHead');
+    const headline = document.createElement('h3');
+    headline.textContent = playerName;
+}
+
+//players logs 
+function logGame() {
+    const log = document.createElement('div')
+}
+
+function game() {
+    var player1 = new Player('Player1')
+    var player2 = new ComputerAI('ComputerAI')
+
+    const divBoard = document.querySelector('#divBoard')
+    divBoard.appendChild(showBoard(player1.name))
+    divBoard.appendChild(showBoard(player2.name))
+
+    let activePlayer = player1;
+    let oppenentPlayer = player2;
+
+    nextTurn(activePlayer, oppenentPlayer);
+}
+
+function nextTurn(activePlayer, oppenentPlayer) {
+    if (activePlayer.name === 'ComputerAI') {
+        activePlayer.attack(oppenentPlayer);
+        nextTurn(oppenentPlayer, activePlayer);
+    } else {
+        const cells = document.querySelectorAll('td.cell');
+        cells.forEach(cell => {
+            if(!cell.classList.contains('attacked')) {
+                
+                cell.addEventListener('click', (e) => {
+                    const cor = e.target.id
+                    activePlayer.attack(oppenentPlayer, cor[1], cor[2])
+                    nextTurn(oppenentPlayer, activePlayer)
+                }, {once: true})   
+            }         
+        });
+    }
+}
+
+export { mainmenu, showBoard, maingame }
